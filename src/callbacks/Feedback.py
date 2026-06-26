2a7aef345e9914005519e83950d6d3a4329d2915
Frank Bi <frank.bi@mail.utoronto.ca>
2026-06-26 17:20:59 -0600

    Updated documentation, refactored feedbacks for modularity

--- src/callbacks/Feedback.py
+++ src/callbacks/Feedback.py
@@ -1,14 +1,23 @@
 class Feedback:
-    
+
     def __init__(self, audio_processor) -> None:
+        """
+        audio_processor: The AudioProcessor instance that is using this feedback.
+        """
         self.audio_processor = audio_processor
+
+    def callback(self, indata, outdata, frames, time, status):
+        if status:
+            print(status)
+        self.callback(indata, outdata, frames, time)
 
     def callback(self, indata, outdata, frames, time):
         """
-        indata:  NumPy array of shape (BLOCK_SIZE, 1) containing mono microphone input
-        outdata: NumPy array of shape (BLOCK_SIZE, 2) containing stereo output
+        indata:  NumPy array of shape (frames, 1) containing mono microphone input.
+        outdata: NumPy array of shape (frames, 2) containing stereo output.
+        frames:  The number of audio frames in the input and output arrays.
         """
         raise NotImplementedError("This is an abstract parent class.")
-    
+
     def __str__(self) -> str:
         return "base feedback"
