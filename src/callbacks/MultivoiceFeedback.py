b40b8f8592cfae09cfdb41e9a91398f7e4281e68
Frank Bi <frank.bi@mail.utoronto.ca>
2026-06-26 17:21:51 -0600

    Fixed delay bug, added option for multiple voices

--- src/callbacks/MultivoiceFeedback.py
+++ src/callbacks/MultivoiceFeedback.py
@@ -6,14 +6,20 @@ from collections import deque
 
 class MultivoiceFeedback(Feedback):
 
-    def __init__(self, audio_processor) -> None:
+    def __init__(self, audio_processor, num_voices=1, sec_delay=0.1) -> None:
+        """
+        audio_processor: The AudioProcessor instance that is using this feedback.
+        num_voices:      The number of additional voices to add to the output.
+        sec_delay:       The delay (in seconds) between each voice.
+        """
+        
         super().__init__(audio_processor)
         
-        self.DELAY = self.audio_processor.SAMPLE_RATE
-        self.num_voices = 2
+        self.delay = int(self.audio_processor.SAMPLE_RATE * sec_delay) # DELAY = SAMPLE_RATE means a 1 second delay between each voice
+        self.num_voices = num_voices # Number of voices to add (not including the original, cannot be <0)
         
         # Keep track of audio history
-        self.BUFFER_SIZE = self.num_voices * (self.audio_processor.BLOCK_SIZE + self.DELAY)
+        self.BUFFER_SIZE = self.num_voices * (self.audio_processor.BLOCK_SIZE + self.delay)
         self.audio_buffer = deque(maxlen=self.BUFFER_SIZE)
         self.audio_buffer.extend(np.zeros(self.BUFFER_SIZE))
 
@@ -22,15 +28,14 @@ class MultivoiceFeedback(Feedback):
         
         data = indata.squeeze()
         self.audio_buffer.extend(data)
-        self.audio_buffer.extend(np.zeros(self.DELAY))
         history = np.array(self.audio_buffer)
         
-        for i in range(1, self.num_voices): # For every voice that is added
-            n = self.audio_processor.BLOCK_SIZE
-            outdata[:] = outdata + history[(i-1)*n:i*n, np.newaxis]
+        skip = frames + self.delay
+        for i in range(0, self.num_voices): # Repeat past input, starting from the most distant past
+            outdata[:] = outdata + history[(i*skip):(i*skip)+frames, np.newaxis]
         
-        outdata[:] = outdata / 2 # Reduce volume
+        outdata[:] = outdata / (self.num_voices + 1) # Reduce volume
         
 
     def __str__(self) -> str:
-        return "static feedback"
+        return "multivoice feedback"
